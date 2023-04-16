eca36e99699958e056c8d06978510e75c73f6051
vishnucprasad <mail@vishnucprasad.in>
2023-04-16 11:47:30 +0530

    Modified sendOtp and verifyOtp services for test purposes

--- services/verify.service.js
+++ services/verify.service.js
@@ -2,11 +2,11 @@
 
 const User = require("../models/user.model");
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const serviceSid = process.env.TWILIO_SERVICE_SID;
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const serviceSid = process.env.TWILIO_SERVICE_SID;
 
-const client = require("twilio")(accountSid, authToken);
+// const client = require("twilio")(accountSid, authToken);
 
 const isExistingUser = async (countryCode, phone) => {
     try {
@@ -20,14 +20,17 @@ const isExistingUser = async (countryCode, phone) => {
 
 const sendOtp = async (countryCode, phone) => {
     try {
-        const verification = await client.verify.v2
-            .services(serviceSid)
-            .verifications.create({
-                to: `${countryCode}${phone}`,
-                channel: "sms",
-            });
+        // const verification = await client.verify.v2
+        //     .services(serviceSid)
+        //     .verifications.create({
+        //         to: `${countryCode}${phone}`,
+        //         channel: "sms",
+        //     });
 
-        return verification;
+        // return verification;
+
+        // for test purposes
+        return true;
     } catch (e) {
         throw e;
     }
@@ -35,14 +38,22 @@ const sendOtp = async (countryCode, phone) => {
 
 const verifyOtp = async (countryCode, phone, otp) => {
     try {
-        const verificationChecks = await client.verify.v2
-            .services(serviceSid)
-            .verificationChecks.create({
-                to: `${countryCode}${phone}`,
-                code: otp,
-            });
+        // const verificationChecks = await client.verify.v2
+        //     .services(serviceSid)
+        //     .verificationChecks.create({
+        //         to: `${countryCode}${phone}`,
+        //         code: otp,
+        //     });
 
-        return verificationChecks.status === "approved";
+        // return verificationChecks.status === "approved";
+
+        // for test purposes
+        const zeroPad = (num, places) => String(num).padStart(places, "0");
+
+        const date = new Date().getDate();
+        const month = new Date().getMonth() + 1;
+
+        return `${zeroPad(date, 2)}${zeroPad(month, 2)}` === otp;
     } catch (e) {
         throw e;
     }
